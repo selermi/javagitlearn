4b0aa6925c8222e61933aaa446ba04f0c780bedc
SalihaKoc <salihakc90@gmail.com>
2022-10-02 01:11:31 +0300

    salihayeni

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,14 @@
 public class hello {
     public static void main(String[] args) {
 
+
+        System.out.println("Hello of Git");
+
+
         System.out.println("Hello world");
+        System.out.println("Hello Koray");
+
+        System.out.println("bakalım olacak mı?");
 
     }
 }
